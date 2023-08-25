5a34d24a48cfec9f5453e398151cbc3c1ce9b524
GeorgeSKalad <georgekalad78@gmail.com>
2023-08-25 12:56:42 -0400

    app.py

--- app.py
+++ app.py
@@ -61,6 +61,30 @@ def precipitation():
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+
+    session.close()
+
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+
+    session.close()
+    
+    temps = list(np.ravel(results))
+
+    return jsonify(temps=temps)
+
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
